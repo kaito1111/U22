b2157b4f7728a7904ffd7f18355e5ae04715ec60
okadakoushi <kbc19b06@stu.kawahara.ac.jp>
2019-12-24 14:29:23 +0900

    2d

--- GameTemplate/Game/GPUView/ShaderResourceView.cpp
+++ GameTemplate/Game/GPUView/ShaderResourceView.cpp
@@ -21,16 +21,37 @@ namespace myEngine {
 		//無効なデータフラグ
 		m_isValid = false;
 	}
-	/// <summary>
-	/// 
-	/// </summary>
-	/*
+
 	bool ShaderResourceView::Create(StructuredBuffer& structuredBuffer)
 	{
 		//データの解放(初期化)
 		Release();
+		//ID3D11バッファーの取得
 		ID3D11Buffer* pBuf = structuredBuffer.GetBody();
+		if (pBuf != nullptr) {
+			//バッファリソースの情報
+			D3D11_BUFFER_DESC descBuf;
+			/*
+			引数で渡した値を0で埋めてくれる関数です
+			詳細については参考メモに書いてあるので見てください
+			*/
+			//恐らく初期化処理です
+			ZeroMemory(&descBuf, sizeof(descBuf));
+			//pBufで初期化したバッファリソースの値を持ってくる？
+			pBuf->GetDesc(&descBuf);
 
+			//シェーダーリソースビュー
+			D3D11_SHADER_RESOURCE_VIEW_DESC desc;
+			//初期化
+			ZeroMemory(&desc, sizeof(desc));
+			//リソースがバッファーですよっていうフラグをたてる
+			desc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+			//最初にビューに適用するインデックス(頂点)の番号
+			desc.BufferEx.FirstElement = 0;
+
+
+			desc.Format = DXGI_FORMAT_UNKNOWN;
+		}
+		return false;
 	}
-	*/
 }
