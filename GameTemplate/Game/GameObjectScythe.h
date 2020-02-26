667847b19975537b05c210b56f563fa9e6077888
kurusu <1>
2020-02-26 10:07:08 +0900

    鎌まで

--- GameTemplate/Game/GameObjectScythe.h
+++ GameTemplate/Game/GameObjectScythe.h
@@ -6,8 +6,17 @@ public:
 	~GameObjectScythe();
 	bool Start();
 	void Update();
+	void Draw();
 
+	void Move();//振り子運動
 private:
+	SkinModel m_model;							 //スキンモデル
+	CVector3 m_pos = CVector3::Zero();			 //座標
+	CQuaternion m_rot = CQuaternion::Identity(); //回転量
+	float rot = 0.0f;                            //回転してる量
+	float spinSpeed = 0.0f;						 //回転速度
+	bool spinChange = false;                     //回転方向を切り替えるフラグ
+	bool speedChange = false;                    //回転速度を切り替えるフラグ
 
 };
 
