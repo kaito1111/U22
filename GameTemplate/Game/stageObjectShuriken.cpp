9bbc183e21cb0c2bc090015917aee7f109dcbebd
kurusu <1>
2020-03-16 20:29:56 +0900

    ファインドゴー重たいどうしよ。左右に動き続ける何かを作った。当たったらプレイヤーが死ぬようになるけどファインドゴー重たいどうしよ

--- GameTemplate/Game/stageObjectShuriken.cpp
+++ GameTemplate/Game/stageObjectShuriken.cpp
@@ -1,11 +1,16 @@
 #include "stdafx.h"
 #include "stageObjectShuriken.h"
 
-
+//動作確認とかするためのコンストラクタ。最終消す。
 stageObjectShuriken::stageObjectShuriken()
 {
+	
 }
 
+//レベル配置用のコンストラクタ
+//stageObjectShuriken::stageObjectShuriken(const wchar_t * modelName, CVector3 pos, CQuaternion rot)
+//{
+//}
 
 stageObjectShuriken::~stageObjectShuriken()
 {
@@ -13,10 +18,74 @@ stageObjectShuriken::~stageObjectShuriken()
 
 bool stageObjectShuriken::Start()
 {
+	//モデルどうしようか。
+	m_model.Init(L"Assets/modelData/shuriken.cmo");
+
 	return true;
+}
+
+void stageObjectShuriken::Draw()
+{
+	m_model.Draw(
+		g_camera3D.GetViewMatrix(),
+		g_camera3D.GetProjectionMatrix()
+	);
 }
 
 void stageObjectShuriken::Update()
 {
+	//ここに書いてあるのどっかで見たことあるとか言ってはいけない。
+	const float speedUPLimit = 10;  //スピードの上限
+	const float speedDownLimit = 0;//スピードの下限
+	const float speed = 0.1;
 
+	//migi
+	if (UDPos == false) {
+
+		//hidari
+		if (speedDown == false) {
+			moveSpeed += speed;
+			//一定値まで行ったら加減速フラグを切り替える
+			if (moveSpeed >= speedUPLimit) {
+				speedDown = true;
+			}
+		}
+
+		//減速
+		if (speedDown == true) {
+			moveSpeed -= speed;
+
+			if (moveSpeed <= speedDownLimit) {
+				speedDown = false;				//一定値まで行ったらフラグを切り替える。
+				UDPos = true;					//下がり切っているので左右フラグも切り替える
+				moveSpeed = speedDownLimit;		//スピードを0に合わせる
+			}
+		}
+		m_pos.x += moveSpeed;
+	}
+
+	//migi
+	if (UDPos == true) {
+
+		//加速
+		if (speedDown == false) {
+			moveSpeed += speed;
+			//一定値まで行ったらフラグを切り替える
+			if (moveSpeed >= speedUPLimit) {
+				speedDown = true;
+			}
+		}
+
+		//減速
+		if (speedDown == true) {
+			moveSpeed -= speed;
+			//一定値まで行ったらフラグを切り替える
+			if (moveSpeed <= speedDownLimit) {
+				speedDown = false;
+				UDPos = false;
+				moveSpeed = speedDownLimit;
+			}
+		}
+		m_pos.x -= moveSpeed;
+	}
 }
--- GameTemplate/Game/stageObjectShuriken.h
+++ GameTemplate/Game/stageObjectShuriken.h
@@ -1,16 +1,25 @@
 #pragma once
 class stageObjectShuriken : public IGameObject
 {
-public:
-	stageObjectShuriken();
-	~stageObjectShuriken();
-	//レベル君で呼ぶからパブリック！
-	bool Start();
-	void Update();
-private:
-	SkinModel m_model;
-	CVector3 m_pos = CVector3::Zero();
-	CQuaternion m_rot = CQuaternion::Identity();
+	class Player;
+		//サイズ200*200*20
+	public:
+		stageObjectShuriken();
+		//stageObjectShuriken(const wchar_t* modelName, CVector3 pos, CQuaternion rot);
+		~stageObjectShuriken();
+
+		void Draw();
+		bool Start();
+		void Update();
+		void Move();
+	private:
+		SkinModel m_model;								//スキンモデル
+		CVector3 m_pos = CVector3::Zero();				//座標
+		CQuaternion m_rot = CQuaternion::Identity();	//回転
+		bool UDPos = false;								//右端左端
+		bool speedDown = false;							//スピードを上げたり下げたりする
+		float moveSpeed = 0;                            //速度。左右に動く
+		bool sLimit = false;		    				//動いた量
 
 
 };
