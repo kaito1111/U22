ce64ab783d2b4925196b5e4b7c9d2b3ea2b744d4
kurusu <1>
2020-03-30 09:54:31 +0900

    MoveFloor2(横移動式）追加。コピペ。

---
+++ GameTemplate/Game/MoveFloor2.h
@@ -0,0 +1,28 @@
+#pragma once
+#include"physics/PhysicsStaticObject.h"
+class MoveFloor2 : public IGameObject
+{
+//左右に動くヤツやで。
+public:
+	MoveFloor2();
+	~MoveFloor2();
+
+private:
+	void Draw();
+	bool Start();
+	void Update();
+	void Move();
+	void SetPosition(const CVector3& pos) {
+		m_pos = pos;
+	}
+
+	SkinModel m_model;								//スキンモデル
+	CVector3 m_pos = CVector3::Zero();				//座標
+	CQuaternion m_rot = CQuaternion::Identity();	//回転
+	bool UDPos = false;								//上がり切ってるか下がり切ってるか
+	bool speedDown = false;							//スピードを上げたり下げたりする
+	float moveSpeed = 0;                            //速度。上がったり下がったりする
+	bool sLimit = false;		    				//動いた量
+	PhysicsStaticObject m_phyStaticObject;          //静的物理オブジェクト
+};
+
