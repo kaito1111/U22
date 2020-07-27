38daa42cffbdac85a87ec0d77ddb7eea3f970dda
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2020-07-27 11:55:10 +0900

    debugMan元に戻す

---
+++ GameTemplate/Game/Debug/DebugMan.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include "DebugMan.h"
+#include "Stage.h"
+
+namespace myEngine {
+
+	DebugMan::DebugMan()
+	{
+		//ゴールの位置
+		//m_pos = { -358.0f,925.0f,0.0f };
+		m_pos = CVector3::Zero();
+		m_skinModel.Init(L"Assets/modelData/unityChan.cmo");
+	
+		//シャドウ関連の初期化処理
+		{
+			//グラフィックスエンジンからシャドウマップを取得
+			m_shadowMap = g_graphicsEngine->GetShadowMap();
+			//影を落とすカメラの座標
+			//m_lightCameraPosition = { 0.0f, 4000.0f, 0.0f };
+			//影を落とす注視点の座標
+			m_lightCameraTarget = CVector3::Zero();;/*CVector3::Zero();*/
+		}
+	}
+
+	DebugMan::~DebugMan()
+	{
+
+	}
+
+	void DebugMan::Update()
+	{
+		if (GetAsyncKeyState('A')) {
+			m_pos.x += 5;
+		}
+		else if(GetAsyncKeyState('D')){
+			m_pos.x -= 5;
+		}
+
+		//ワールド行列更新
+		m_skinModel.UpdateWorldMatrix(m_pos, m_rot, m_scale);
+	}
+
+	void DebugMan::PreRender()
+	{
+		//シャドウマップにレンダリング
+		{
+			//描画設定のバックアップ
+			//m_shadowMap->BiginRender();
+			//シャドウキャスター登録
+			m_shadowMap->RegistShdowCaster(&m_skinModel);
+			//シャドウマップ用の描画設定に切り替えて
+			//登録されているシャドウキャスターの影を描画
+			//m_shadowMap->RenderToShadowMap();
+			//描画設定をもとに戻す
+			//m_shadowMap->EndRender();
+		}
+	}
+
+
+
+	void DebugMan::Draw()
+	{
+		//通常レンダリング
+		{
+			m_skinModel.Draw(
+				g_camera3D.GetViewMatrix(),
+				g_camera3D.GetProjectionMatrix(),
+				0,
+				g_graphicsEngine->GetShadowMap()->GetLightViewMatirx(),
+				g_graphicsEngine->GetShadowMap()->GetLightProjMatirx()
+			);
+		}
+	}
+}
---
+++ GameTemplate/Game/Debug/DebugMan.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Shadow/ShadowMap.h"
+
+namespace myEngine {
+	/// <summary>
+	/// ！！デバッグマン！！
+	/// 能力ⅰ：シャドウマップの作成ができる。
+	/// 能力ⅱ：影を出せる。
+	/// </summary>
+	/// <remarks>
+	/// エンジンの機能追加の際など既存のPlayerとか使うのは、
+	/// 気が引けるので作成。
+	/// モデルも後々かえて、アニメーションも付けたい。
+	/// </remarks>
+	class DebugMan : public IGameObject
+	{
+	public:
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		DebugMan();
+		/// <summary>
+		/// デストラクタ
+		/// </summary>
+		~DebugMan();
+		/// <summary>
+		/// スタート
+		/// </summary>
+		bool Start() { return true; };
+		/// <summary>
+		/// 更新
+		/// </summary>
+		void Update();
+		/// <summary>
+		/// Drawより早い描画
+		/// </summary>
+		void PreRender();
+		/// <summary>
+		/// 描画
+		/// </summary>
+		void Draw();
+	private:
+		CVector3 m_pos = CVector3::Zero();				//デバッグマンの位置
+		CVector3 m_scale = CVector3::One();				//デバッグマンの拡大率
+		CQuaternion m_rot = CQuaternion::Identity();	//デバッグマンの回転軸
+		CVector3 m_lightCameraPosition;					//ライトカメラの視点位置
+		CVector3 m_lightCameraTarget;					//ライトカメラの注視点位置
+		SkinModel m_skinModel;							//デバッグマンのスキンモデル
+		ShadowMap* m_shadowMap = nullptr;				//シャドウマップ
+	};
+}
