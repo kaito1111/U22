8588ea1847c2ca2548c996270f56b381f444db4f
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2020-08-05 14:54:15 +0900

    a

--- GameTemplate/Game/character/CharacterController.cpp
+++ GameTemplate/Game/character/CharacterController.cpp
@@ -21,7 +21,8 @@ namespace {
 		//衝突したときに呼ばれるコールバック関数。
 		virtual	btScalar	addSingleResult(btCollisionWorld::LocalConvexResult& convexResult, bool normalInWorldSpace)
 		{
-			if (convexResult.m_hitCollisionObject == me
+			if (convexResult.m_hitCollisionObject == me 
+				|| convexResult.m_hitCollisionObject->getUserIndex() == enCollisionAttr_Character
 				) {
 				//自分に衝突した。
 				return 0.0f;
